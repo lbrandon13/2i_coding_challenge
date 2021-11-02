edcce3d3349ad8bf53b5fab50f2fd96ddf7b33b7
lbrandon13 <31885900+lbrandon13@users.noreply.github.com>
2021-11-02 15:06:58 +0000

    Added comments of challenge description

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello, World!");
+        
+        /* Create 3 functions:
+         * 1. Iterate multiples of 7 up to 100 
+         * 2. Iterate multiples of 8 up to 200
+         * 3. Iterate multiples of 9 up to 300
+        */ 
+
     }
 }

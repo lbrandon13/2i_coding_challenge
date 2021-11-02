7f95749d95d1a36dd7216fc4185628ed2bc31719
lbrandon13 <31885900+lbrandon13@users.noreply.github.com>
2021-11-02 15:16:19 +0000

    Added iteratives of 7 function up to 100

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
 public class Main {
+
+    // Iterate multiples of 7
+    static int iterativesOf7(int n){
+        return n * 7;
+    }
     public static void main(String[] args){
         
         /* Create 3 functions:
@@ -6,6 +11,11 @@ public class Main {
          * 2. Iterate multiples of 8 up to 200
          * 3. Iterate multiples of 9 up to 300
         */ 
-
+        int n = 1;
+        while (iterativesOf7(n) <= 100) {
+            System.out.println(iterativesOf7(n));
+            n++;
+        }
+        
     }
 }
